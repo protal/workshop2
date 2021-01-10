af5e6cc59a5e26eb1a2013db56c1ef51c7e2a493
protal <slipknot.opal@gmail.com>
2021-01-10 14:16:26 +0700

    add create , get type , sigle or double variable

---
+++ variable/create.py
@@ -0,0 +1,12 @@
+name = "harit kongthong"
+age = 24
+weight = 80.0
+# boolen | can is has
+can_walk = True
+can_run = False
+
+print(name)
+print(age)
+print(weight)
+print(can_walk)
+print(can_run)
