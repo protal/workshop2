a33337b59c51e34f4916cae94a0be404d529a56d
protal <slipknot.opal@gmail.com>
2021-01-10 14:33:30 +0700

    add vairable name

---
+++ variable/name.py
@@ -0,0 +1,12 @@
+#สามารถประกาศได้
+myvar = "Por"
+my_var = "Por"
+_my_var = "Por"
+myVar = "Por"
+MYVAR = "Por"
+myvar2 = "Por"
+
+#ไม่สามารถประกาศได้
+2myvar = "Por"
+my-var = "Por"
+my var = "Por"
